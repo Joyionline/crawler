c5452f3d14c8c3f0a1f97abe1f60dcacf9f5b4ab
Joyionline <hhaz1dyy888@vip.qq.com>
2019-04-19 21:04:23 +0800

    Init file

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main()  {
-	
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/text/transform"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+)
+
+func main() {
+	resp, err := http.Get("https://www.zhenai.com/zhenghun")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	// 解决编码问题
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("获取的数据是：%s\n", all)
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
